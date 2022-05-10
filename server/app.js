f1d9efc617ef2b062da6f4aed0b3df7b43c6c905
unknown <maftunacreator19@gmail.com>
2022-05-10 18:56:40 +0500

    app.js changed

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 app.use(cors());
+app.use(allowCrossDomain);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
